48ec393c4fe41023ecbb36dc880f7295b4f86b78
VictorAraujo03 <victoraraujo.1079@aluno.saojudas.br>
2023-03-28 16:52:37 -0300

    valeu a versão mais nova da branchconfusão

---
+++ teste.py
@@ -0,0 +1,8 @@
+x = input("O que devo fazer?")
+
+if(x == "teste"):
+    print("teste")
+else:
+    nome = input("Digite seu nome")
+    apelido = input("Digite seu sobrenome:")
+    print(nome + apelido)
